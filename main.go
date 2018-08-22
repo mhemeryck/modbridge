4ac444e4603b3e35795aca527c8354fc5c1847b0
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2018-08-22 23:07:50 +0200

    Adds simple config handling based on viper
    Adds simple config with defaults based on viper. As dep is not able to
    copy in the config file directly, we instead rely on env vars and just
    skip this for now.

--- main.go
+++ main.go
@@ -4,15 +4,27 @@ import (
 	"fmt"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/goburrow/modbus"
+	"github.com/spf13/viper"
 	"log"
 	"time"
 )
 
+func readConfig() {
+	viper.AutomaticEnv()
+	// defaults
+	viper.SetDefault("mqtt_broker_uri", "tcp://raspberrypi.lan:1883")
+	viper.SetDefault("mqtt_client_id", "unipusher")
+	viper.SetDefault("modbus_server_uri", "unipi.lan:502")
+}
+
 func main() {
+	// Read configuration
+	readConfig()
+
 	// MQTT client
-	opts := mqtt.NewClientOptions().AddBroker("tcp://raspberrypi.lan:1883").SetClientID("beppu")
-	mqtt_client := mqtt.NewClient(opts)
-	token := mqtt_client.Connect()
+	opts := mqtt.NewClientOptions().AddBroker(viper.GetString("mqtt_broker_uri")).SetClientID(viper.GetString("mqtt_client_id"))
+	mqttClient := mqtt.NewClient(opts)
+	token := mqttClient.Connect()
 	if token.Wait() && token.Error() != nil {
 		panic(token.Error())
 	}
@@ -21,7 +33,7 @@ func main() {
 	previous := false
 	current := false
 	state := "OFF"
-	client := modbus.TCPClient("unipi.lan:502")
+	client := modbus.TCPClient(viper.GetString("modbus_server_uri"))
 	for true {
 		results, err := client.ReadDiscreteInputs(100, 1)
 		if err != nil {
@@ -35,7 +47,7 @@ func main() {
 				} else {
 					state = "OFF"
 				}
-				mqtt_client.Publish("unipi/digital_input/21", 0, false, state)
+				mqttClient.Publish("unipi/digital_input/21", 0, false, state)
 			}
 			fmt.Printf("%s: %d\n", time.Now().Format(time.UnixDate), results[0])
 			previous = current

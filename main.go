4518377605da12ed8251ba7e28215733f906de8b
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2018-08-24 13:42:10 +0200

    Adds checking for client availability on start
    Use concurrent checking on availability of mqtt and modbus client. Block
    on both being available through a semaphore-style use of channels.

--- main.go
+++ main.go
@@ -23,6 +23,38 @@ var addressMap = map[uint16]string{
 	101: "Digital input 2.2",
 	102: "Digital input 2.3",
 	103: "Digital input 2.4",
+	104: "Digital input 2.5",
+	105: "Digital input 2.6",
+	106: "Digital input 2.7",
+	107: "Digital input 2.8",
+	108: "Digital input 2.9",
+	109: "Digital input 2.10",
+	110: "Digital input 2.11",
+	111: "Digital input 2.12",
+	112: "Digital input 2.13",
+	113: "Digital input 2.14",
+	114: "Digital input 2.15",
+	115: "Digital input 2.16",
+	116: "Digital input 2.17",
+	117: "Digital input 2.18",
+	118: "Digital input 2.19",
+	119: "Digital input 2.20",
+	120: "Digital input 2.21",
+	121: "Digital input 2.22",
+	122: "Digital input 2.23",
+	123: "Digital input 2.24",
+	124: "Digital input 2.25",
+	125: "Digital input 2.26",
+	126: "Digital input 2.27",
+	127: "Digital input 2.28",
+	128: "Digital input 2.29",
+	129: "Digital input 2.30",
+}
+
+// Input represents any modbus input
+type Input interface {
+	Update() error
+	Slug() string
 }
 
 // DigitalInput represents simple digital toggle
@@ -35,15 +67,23 @@ type DigitalInput struct {
 	mode        int
 }
 
+func (input *DigitalInput) rising() bool {
+	return input.current && !input.previous
+}
+
+func (input *DigitalInput) falling() bool {
+	return !input.current && input.previous
+}
+
 // Update poll the modbus server for an updated value
 func (input *DigitalInput) Update(modbusClient modbus.Client, mqttClient mqtt.Client) error {
 	results, err := modbusClient.ReadDiscreteInputs(input.address, 1)
 	input.previous, input.current = input.current, results[0]&0x01 != 0
 	if input.current != input.previous {
 		input.lastChange = time.Now()
-		if (input.mode == NO && input.current && !input.previous) || (input.mode == NC && !input.current && input.previous) {
+		if (input.mode == NO && input.rising()) || (input.mode == NC && input.falling()) {
+			mqttClient.Publish(input.Slug(), 0, false, "trigger")
 			fmt.Printf("%s, %s: %v\n", input.Slug(), input.lastChange.Format(time.UnixDate), input.current)
-			mqttClient.Publish(input.Slug(), 0, false, "ON")
 		}
 	}
 	return err
@@ -65,32 +105,70 @@ func readConfig() {
 	viper.SetDefault("modbus_server_uri", "unipi.lan:502")
 }
 
+type empty struct{}
+
 func main() {
 	// Read configuration
 	readConfig()
 
+	sem := make(chan empty, 2)
+
 	// MQTT client
 	opts := mqtt.NewClientOptions().AddBroker(viper.GetString("mqtt_broker_uri")).SetClientID(viper.GetString("mqtt_client_id"))
 	mqttClient := mqtt.NewClient(opts)
-	token := mqttClient.Connect()
-	if token.Wait() && token.Error() != nil {
-		panic(token.Error())
-	}
+	go func() {
+		for {
+			token := mqttClient.Connect()
+			if token.Wait() && token.Error() != nil {
+				fmt.Printf("Can't connect to MQTT host, retrying ...\n")
+			} else {
+				sem <- empty{}
+				break
+			}
+		}
+	}()
 
-	// modbus client
-	modbusClient := modbus.TCPClient(viper.GetString("modbus_server_uri"))
+	// Modbus client
+	handler := modbus.NewTCPClientHandler(viper.GetString("modbus_server_uri"))
+	go func() {
+		for {
+			err := handler.Connect()
+			if err != nil {
+				fmt.Print("Can't connect to Modbus server, retrying ...\n")
+			} else {
+				sem <- empty{}
+				break
+			}
+		}
+	}()
+	defer handler.Close()
+	modbusClient := modbus.NewClient(handler)
+
+	// Wait for concurrent tasks to finish
+	for i := 0; i < cap(sem); i++ {
+		<-sem
+	}
 
 	// Initialize list of inputs
 	var inputs []DigitalInput
 	for address, description := range addressMap {
 		inputs = append(inputs, DigitalInput{address: address, description: description, previous: false, current: false, mode: NO})
 	}
+
+	// Infinite loop
+	sem = make(chan empty, 8)
 	for {
 		for k := range inputs {
-			err := inputs[k].Update(modbusClient, mqttClient)
-			if err != nil {
-				log.Fatal(err)
-			}
+			go func(k int) {
+				err := inputs[k].Update(modbusClient, mqttClient)
+				if err != nil {
+					log.Fatal(err)
+				}
+				sem <- empty{}
+			}(k)
+		}
+		for i := 0; i < cap(sem); i++ {
+			<-sem
 		}
 	}
 }

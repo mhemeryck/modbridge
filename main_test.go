165b2ea504aec706a82ab3ca751c6afe1e00acb2
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2018-09-10 23:11:21 +0200

    Adds data structures to parse yaml config
    Adds setup to gather all setup-related settings from yaml config instead
    of via ENV vars. This makes more sense for this setup where we'd like to
    support more devices. Rest of the setup is more or less still the same;
    digital inputs are still polled directly.
    
    Adds an example configuration file including all the coils from a
    specific device, i.e. the Unipi Neuron L303.

--- main_test.go
+++ main_test.go
@@ -6,26 +6,8 @@ import (
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/goburrow/modbus"
 	"github.com/mhemeryck/modbridge/mocks"
-	"github.com/spf13/viper"
 	"github.com/stretchr/testify/mock"
 )
-
-func TestSlug(t *testing.T) {
-	input := DigitalInput{description: "name with spaces"}
-	if input.Slug() != "name_with_spaces" {
-		t.Fail()
-	}
-}
-
-func TestReadConfig(t *testing.T) {
-	readConfig()
-	keys := [3]string{"mqtt_broker_uri", "mqtt_client_id", "modbus_server_uri"}
-	for _, key := range keys {
-		if viper.GetString(key) == "" {
-			t.Fail()
-		}
-	}
-}
 
 func TestRising(t *testing.T) {
 	cases := []struct {
